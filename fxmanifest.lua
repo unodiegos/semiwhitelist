e0077a78a178ab5806de1f00997f8ea6ea3fed84
Diego <amozaka06@outlook.com>
2025-04-13 03:56:37 +0200

    Created fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Diego'
+description 'Semiwhitelist system for [ESX]'
+
+shared_script {'shared/config.lua'}
+client_script {'client/*.lua'}
+server_script {'server/*.lua'}
