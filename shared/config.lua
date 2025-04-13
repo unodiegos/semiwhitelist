753f773de85ffd32733a06a44e2fe9b350f0f79c
Diego <amozaka06@outlook.com>
2025-04-13 03:57:07 +0200

    Created config.lua

---
+++ shared/config.lua
@@ -0,0 +1,9 @@
+Config = {
+    Engine = '', -- Base Export  | exampel : 'es_extended', 'server_base'
+
+    DiscordRoles = {
+        krimRole = '', -- Role ID for Criminal
+        pedRole = '', -- Role ID for Ped 
+        whitelistRole = '', -- Role ID for Whitelist 
+        }
+}
